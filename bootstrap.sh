a647049f2f6ca1626f53f83463d36160cb8bff36
Nils Ortnr <nils.ortner@mivitec.net>
2012-12-19 20:51:00 +0100

    bootstrap.sh erstellt

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+
+cd /usr/src; wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb &&  dpkg -i puppetlabs-release-precise.deb
+
+apt-get update
+apt-get upgrade -y
+apt-get dist-upgrade
+
+apt-get install git puppet
